d5672d21dfdd4465dc17c3015b788c9d452e1d4f
coffee-marios <mark_theo@yahoo.com>
2023-06-19 13:51:49 +0300

    Write a function that loops through the library (array) and displays each book on a table

--- script.js
+++ script.js
@@ -45,5 +45,32 @@ const JS = new Book(
   "no"
 );
 
-console.log(JS.info());
-console.log(Object.getPrototypeOf(JS));
+function addBookToLibrary() {
+  let myBooks = document.getElementById("books-collection");
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    let new_row = myBooks.insertRow(1);
+    let new_cell;
+    for (let j = 0; j < 4; j++) {
+      new_cell = new_row.insertCell(j);
+      new_cell.innerHTML = Object.values(myLibrary[i])[j];
+      console.log(myLibrary[i]);
+    }
+  }
+}
+
+addBookToLibrary();
+
+// console.log(JS.info());
+// console.log(Object.getPrototypeOf(JS));
+
+// // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+// var cell1 = row.insertCell(0);
+// var cell2 = row.insertCell(1);
+// var cell3 = row.insertCell(2);
+// var cell4 = row.insertCell(3);
+// // Add some text to the new cells:
+// cell1.innerHTML = "NEW CELL1";
+// cell2.innerHTML = "NEW CELL2";
+// cell3.innerHTML = "NEW CELL1";
+// cell4.innerHTML = "NEW CELL2";
