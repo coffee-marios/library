522411086370727b068ad13b2f28499f6e5e99ae
coffee-marios <mark_theo@yahoo.com>
2023-06-21 13:09:17 +0300

    Make the form appear and disappear with the click of a button

--- script.js
+++ script.js
@@ -64,13 +64,14 @@ addBookToLibrary();
 // console.log(JS.info());
 // console.log(Object.getPrototypeOf(JS));
 
-// // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-// var cell1 = row.insertCell(0);
-// var cell2 = row.insertCell(1);
-// var cell3 = row.insertCell(2);
-// var cell4 = row.insertCell(3);
-// // Add some text to the new cells:
-// cell1.innerHTML = "NEW CELL1";
-// cell2.innerHTML = "NEW CELL2";
-// cell3.innerHTML = "NEW CELL1";
-// cell4.innerHTML = "NEW CELL2";
+function showForm() {
+  let statusForm = document.getElementById("addBookForm");
+  console.log(statusForm.style.display);
+
+  statusForm.style.display === "block"
+    ? (statusForm.style.display = "none")
+    : (statusForm.style.display = "block");
+}
+
+const showFormButton = document.getElementById("showForm");
+showFormButton.addEventListener("click", showForm);
