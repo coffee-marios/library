d80a0e6a4aa9c292d3ea6581d5aa235dd702edfb
coffee-marios <mark_theo@yahoo.com>
2023-06-30 12:10:58 +0300

    Add JS validation

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let myLibrary = [
     title: "The Black Swan: The Impact of the Highly Improbable",
     author: "Nassim Nicholas Taleb",
     pages: 394,
-    read: "yes",
+    read: "YES",
     id_book: 0,
   },
   {
@@ -11,26 +11,26 @@ let myLibrary = [
       "Very Bad People: The Inside Story of the Fight Against the World’s Network of Corruption",
     author: "Patrick Alley",
     pages: 336,
-    read: "no",
+    read: "NO",
     id_book: 1,
   },
   {
     title: "People Hacker",
     author: "Jenny Radcliffe",
     pages: 303,
-    read: "no",
+    read: "NO",
     id_book: 2,
   },
 ];
 
 let id_book = 2;
 
-const Book = function (title, author, pages, read, id_book) {
+const Book = function (title, author, pages, read, id_books) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.id_book = id_book;
+  this.id_books = id_book;
 };
 
 const increment = function () {
@@ -39,92 +39,144 @@ const increment = function () {
   return id_book;
 };
 
-Book.prototype.info = function () {
-  const haveRead = function () {
-    if (this.read === "yes") {
-      return "I have read it.";
-    } else {
-      return "I haven't read it yet.";
-    }
-  };
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${haveRead()}`;
-};
+const newTitle = document.getElementById("ftitle");
+const newAuthor = document.getElementById("fauthor");
+const newPages = document.getElementById("fpages");
+const newRead = document.getElementById("fread");
+
+let test = false;
+
+function testValid() {
+  // The validation of html is skipped with we submit with js
+
+  let testTitle = false;
+  let testPages = false;
+
+  if (newTitle.validity.valueMissing) {
+    newTitle.setCustomValidity("Please, write the title of the book");
+    newTitle.reportValidity();
+  } else {
+    newTitle.setCustomValidity("");
+    testTitle = true;
+  }
+
+  var numbers = /^[-+]?[0-9]+$/;
+  if (newPages.value.match(numbers) || newPages.value === "") {
+    testPages = true;
+  } else if (!newPages.value.match(numbers) && newPages.value !== "") {
+    newPages.setCustomValidity("Please, write a number");
+    newPages.reportValidity();
+    testPages = false;
+  }
+
+  if (testPages === true && testTitle === true) {
+    console.log(
+      `This is a validation test: ${test}\n testTitle: ${testTitle}\n testPages= ${testPages}`
+    );
+    testPages = false;
+    testTitle = false;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+newTitle.addEventListener("input", testValid);
 
 function addBookToLibrary() {
-  if (statusForm.style.display === "block") {
-    statusForm.style.display = "none";
+  let see = testValid();
+
+  if (see) {
+    if (newAuthor.validity.valueMissing) {
+      newAuthor.value = "-";
+    }
+
+    if (newPages.validity.valueMissing) {
+      newPages.value = "-";
+    }
+
+    //console.clear();
+    //console.log(Book.prototype);
+
+    let id_newRow = increment();
+    console.log(`id_newRow: ${id_newRow}, see: ${see}`);
+    //console.clear();
+    console.log(id_newRow, 3333);
+    console.log(id_book);
+    const BookAdd = new Book(
+      newTitle.value,
+      newAuthor.value,
+      newPages.value,
+      id_newRow
+    );
+    console.log(BookAdd);
+
+    myLibrary.push(BookAdd);
+    let myBooks = document.getElementById("books-collection");
+
+    let newRow = myBooks.insertRow(1);
+    newRow.insertCell(0).innerHTML = newTitle.value;
+    newRow.insertCell(1).innerHTML = newAuthor.value;
+    newRow.insertCell(2).innerHTML = newPages.value;
+
+    let read_cell = newRow.insertCell(3);
+    let read_button = document.createElement("BUTTON");
+    let read_text = document.createTextNode(newRead.value);
+    read_button.appendChild(read_text);
+
+    let id_read_button = "r" + id_newRow;
+    read_button.setAttribute("id", id_read_button);
+
+    read_cell.appendChild(read_button);
+    read_button.addEventListener("click", toggleRead);
+
+    newRow.dataset.idRow = id_newRow;
+    console.log(newRow.dataset.idRow);
+
+    const delete_button = document.createElement("button");
+    delete_button.setAttribute("class", "delete_book");
+
+    delete_button.setAttribute("id", id_newRow);
+
+    newRow.insertCell(4).appendChild(delete_button);
+
+    let del_text = document.createTextNode("  ");
+    delete_button.appendChild(del_text);
+
+    delete_button.addEventListener("click", removeBook);
+
+    newTitle.value = "";
+    newAuthor.value = "";
+    newPages.value = "";
+
+    // Remove the form
+    if (statusForm.style.display === "block") {
+      statusForm.style.display = "none";
+    }
+
+    event.preventDefault();
   }
-  const newTitle = document.getElementById("ftitle").value;
-  const newAuthor = document.getElementById("fauthor").value;
-  const newPages = document.getElementById("fpages").value;
-  const newRead = document.getElementById("fread").value;
-
-  console.clear();
-  console.log(Book.prototype);
-
-  let id_newRow = increment();
-  const BookAdd = new Book(newTitle, newAuthor, newRead, id_newRow);
-
-  myLibrary.push(BookAdd);
-  let myBooks = document.getElementById("books-collection");
-
-  let newRow = myBooks.insertRow(1);
-  newRow.insertCell(0).innerHTML = newTitle;
-  newRow.insertCell(1).innerHTML = newAuthor;
-  newRow.insertCell(2).innerHTML = newPages;
-
-  let read_cell = newRow.insertCell(3);
-  let read_button = document.createElement("BUTTON");
-  let read_text = document.createTextNode(`--${newRead}--`);
-  read_button.appendChild(read_text);
-
-  let id_read_button = "r" + id_newRow;
-  read_button.setAttribute("id", id_read_button);
-
-  read_cell.appendChild(read_button);
-  read_button.addEventListener("click", toggleRead);
-
-  newRow.dataset.idRow = id_newRow;
-  console.log(newRow.dataset.idRow);
-
-  const delete_button = document.createElement("button");
-  delete_button.setAttribute("id", id_newRow);
-
-  newRow.insertCell(4).appendChild(delete_button);
-
-  let del_text = document.createTextNode(`--${id_newRow}--`);
-  delete_button.appendChild(del_text);
-
-  delete_button.addEventListener("click", removeBook);
-  event.preventDefault();
 }
 
 function toggleRead(e) {
-  console.clear();
+  // console.clear();
   //console.log(e.target.id, e.target.innerText);
   let id_temp = e.target.id.slice(1);
   //console.log(id_temp, myLibrary[id_temp]["read"]);
-  if (e.target.innerText === "no") {
-    document.getElementById(e.target.id).textContent = "yes";
-    myLibrary[id_temp]["read"] = "yes";
+  if (e.target.innerText === "NO") {
+    document.getElementById(e.target.id).textContent = "YES";
+    myLibrary[id_temp]["read"] = "YES";
   } else {
-    document.getElementById(e.target.id).textContent = "no";
-    myLibrary[id_temp]["read"] = "no";
+    document.getElementById(e.target.id).textContent = "NO";
+    myLibrary[id_temp]["read"] = "NO";
   }
   console.log(myLibrary[id_temp]);
 }
 
-const JS = new Book(
-  "The Principles of Object-Oriented JavaScript",
-  "Nicholas C. Zakas",
-  120,
-  "no"
-);
-
 var myBooks = document.getElementById("books-collection");
 
 function showLibrary() {
-  console.clear();
+  //console.clear();
   for (let i = 0; i < myLibrary.length; i++) {
     let new_row = myBooks.insertRow(1);
     new_row.dataset.idRow = myLibrary[i]["id_book"];
@@ -148,8 +200,9 @@ function showLibrary() {
 
     let del_cell = new_row.insertCell(4);
     var del_button = document.createElement("BUTTON");
-    let del_text = document.createTextNode(`--${myLibrary[i]["id_book"]}--`);
+    let del_text = document.createTextNode("  ");
     del_button.appendChild(del_text);
+    del_button.setAttribute("class", "delete_book");
     del_button.setAttribute("id", myLibrary[i]["id_book"]);
     del_cell.appendChild(del_button);
     del_button.addEventListener("click", removeBook);
@@ -157,7 +210,7 @@ function showLibrary() {
 }
 
 function removeBook(e) {
-  console.clear();
+  //console.clear();
   const id_remove = e.target.id;
 
   console.log(myLibrary);
@@ -169,8 +222,7 @@ function removeBook(e) {
 
 showLibrary();
 
-// console.log(JS.info());
-// console.log(Object.getPrototypeOf(JS));
+// Form for adding more books
 
 let statusForm = document.getElementById("addBookForm");
 
